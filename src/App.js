6a1c29355e7743887ad98051a683a3c0b77ff0ce
V Guardado <auricia.guardado@gmail.com>
2021-02-13 01:21:07 -0800

    wrapper index

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import EmployeeTable from "./components/EmployeeTable";
+import EmployeeRow from "./components/EmployeeTable";
+import Header from "./components/Header";
+import Search from "./components/EmployeeTable";
+import employees from "./employees.json";
+
+function App () {
+  return <HelloDiv />;
+}
+
+export default App;
